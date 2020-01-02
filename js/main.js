959ee911f081f4150fc166f9b9ba30fabf6ec6c8
Vinicius Dietrich <vin.dietrich@gmail.com>
2020-01-02 14:53:25 -0300

    parte1

--- js/main.js
+++ js/main.js
@@ -1,11 +1,21 @@
 function consultaCep() {
+    var cep = document.getElementById("cep").value;
+    var url = "https://viacep.com.br/ws/" + cep + "/json/";
     $.ajax({
-        url: "https://viacep.com.br/ws/89225660/json/",
+        url: url,
         type: "GET",
         success: function(response) {
             console.log(response);
             console.log(response.bairro);
-            alert(response.logradouro);
+            $("#logradouro").html(response.logradouro);
+            $("#bairro").html(response.bairro);
+            $("#localidade").html(response.localidade);
+            $("#uf ").html(response.uf);
+            //document.getElementById("logradouro").innerHTML = response.logradouro
+            //document.getElementById("bairro").innerHTML = response.bairro;
+            //document.getElementById("localidade").innerHTML = response.localidade;
+            //document.getElementById("uf").innerHTML = response.uf;
+            //alert(response.logradouro);
         }
     })
 }
